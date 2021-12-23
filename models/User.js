fac171142f4295cd56536e87e9bd37406f6af09c
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-12-23 22:12:26 +0300

    bcrypt password when creating user

--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
+
+const saltRounds = 10;
 
 const userSchema = new Schema({
 	login: {
@@ -17,5 +20,23 @@ const userSchema = new Schema({
 		required: true,
 	}
 })
+
+userSchema.pre('save', function(next) {
+	if (this.isNew || this.isModified('password')) {
+	  const document = this;
+	  bcrypt.hash(document.password, saltRounds,
+		function(err, hashedPassword) {
+		if (err) {
+		  next(err);
+		}
+		else {
+		  document.password = hashedPassword;
+		  next();
+		}
+	  });
+	} else {
+	  next();
+	}
+  });
 
 module.exports = mongoose.model("User", userSchema);
