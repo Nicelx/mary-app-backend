7e9199be302565176dc8eb32c7c8477a96caa138
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-12-25 21:34:26 +0300

    test login controller with frontend and change a little bit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,22 @@
 const User = require('../models/User');
 
 exports.login = async (req, res, next) => {
-	const {login, password} = req.body;
-	const user = await User.findOne({login});
-	const isLogin = user.isCorrectPassword(password)
-	if (isLogin) {
-		res.json({message: 'fulfilled'})
+	try {
+		const {login, password} = req.body;
+		const user = await User.findOne({login});
+		console.log(user);
+		const isLogin = await user.isCorrectPassword(password)
+		console.log(isLogin);
+		if (isLogin) {
+			res.json({message: 'fulfilled', isLogin})
+		} else {
+			throw new Error('Incorrect login data')
+		}
+	} catch(e) {
+		console.log(e);
 	}
+	
+	
 }
 
 exports.signUp = async (req, res, next) => {
