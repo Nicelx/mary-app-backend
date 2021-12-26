73d986b72fcf13bf5ffc5d8da0bd20fe0b60ae72
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-12-26 21:48:30 +0300

    jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,41 @@
-const User = require('../models/User');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET;
 
 exports.login = async (req, res, next) => {
 	try {
-		const {login, password} = req.body;
-		const user = await User.findOne({login});
-		console.log(user);
-		const isLogin = await user.isCorrectPassword(password)
-		console.log(isLogin);
-		if (isLogin) {
-			res.json({message: 'fulfilled', isLogin})
-		} else {
-			throw new Error('Incorrect login data')
+		const { login, password } = req.body;
+		const user = await User.findOne({ login });
+		if (!user) {
+			const error = new Error("User not found");
+			error.status = 401;
+			throw error;
 		}
-	} catch(e) {
+
+		const isLogin = await user.isCorrectPassword(password);
+		if (isLogin) {
+			const payload = { login: user.login, userId: user._id.toString() };
+
+			const token = jwt.sign(payload, secret, {
+				expiresIn: "1h",
+			});
+			res.status(200).json({ token, userId: user._id.toString() });
+		} else {
+			throw new Error("Incorrect login data");
+		}
+	} catch (e) {
 		console.log(e);
 	}
-	
-	
-}
+};
 
 exports.signUp = async (req, res, next) => {
-	const {name, password, login} = req.body;
+	const { name, password, login } = req.body;
 	console.log(name, password);
 	const user = await new User({
-		name, password, login
-	})
+		name,
+		password,
+		login,
+	});
 	await user.save();
-	res.status(201).json({message:'created'})
-}
+	res.status(201).json({ message: "created" });
+};
